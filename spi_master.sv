3382ed7c34adcb15f30885d3b6fd2ac1d6c8e3b9
jv <jv@softing.com>
2015-04-13 13:57:49 +0200

    Add method for assert equal Use value attribute when assigning value to signal Added generic for number of slaves

--- spi_master.sv
+++ spi_master.sv
@@ -1,6 +1,6 @@
 module spi_master
 #(
-  parameter                              DATA_BYTES = 8
+  parameter                              NUMBER_SLAVES = 1
 )
 (
   input                                  clk,
@@ -10,7 +10,7 @@ module spi_master
   output reg                             sclk,
   output reg                             mosi,
   input                                  miso,
-  output reg                             ss_n,
+  output reg [NUMBER_SLAVES-1:0]         ss_n,
 
   // Avalon Slave interface
   input      [ 1:0]                      avs_address,
@@ -20,12 +20,30 @@ module spi_master
   input      [31:0]                      avs_writedata
 );
 
-/*###########################################################################*/
+/* ========================================================================= */
+/* type definitions                                                          */
+/* ========================================================================= */
 
-reg        start_spi_transfer;
+/* ========================================================================= */
+/* constants                                                                 */
+/* ========================================================================= */
+
+/* ========================================================================= */
+/* signal declarations                                                       */
+/* ========================================================================= */
+
+reg [31:0] slave_select;
 reg [31:0] spi_data;
 
-/*===========================================================================*/
+/* ========================================================================= */
+/* function declarations                                                     */
+/* ========================================================================= */
+
+/* ========================================================================= */
+/* Timing constraints for intermediate signals                               */
+/* ========================================================================= */
+
+/* ######################################################################### */
 
 //always @(*)
 //    stream_out_valid = (stream_in_valid & ~stream_out_endofpacket) | flush_pipe;
@@ -36,23 +54,23 @@ reg [31:0] spi_data;
 always @(posedge clk or negedge reset_n)
 begin
   if (!reset_n) begin
-    start_spi_transfer <= 1'b0;
-    spi_data           <= 'b0;
-    avs_readdata       <= 'b0;
+    slave_select <= 'b0;
+    spi_data     <= 'b0;
+    avs_readdata <= 'b0;
   end
   else begin
     // Read
     if (avs_read) begin
       case (avs_address)
-        0:    avs_readdata <= {31'b0, start_spi_transfer};
+        0:    avs_readdata <= slave_select;
         1:    avs_readdata <= spi_data;
       endcase
     end
     // Write
     else if (avs_write) begin
       case (avs_address)
-        0:    start_spi_transfer <= avs_writedata[0];
-        1:    spi_data           <= avs_writedata;
+        0:    slave_select <= avs_writedata;
+        1:    spi_data     <= avs_writedata;
       endcase
     end
   end
--- test_spi.py
+++ test_spi.py
@@ -3,13 +3,21 @@
 Test bench for SPI Master
 """
 
-import random
 import cocotb
 
 from cocotb.triggers import Timer, RisingEdge , FallingEdge#, ReadOnly
 from cocotb.drivers.avalon import AvalonMaster
 from cocotb.result import TestFailure
 
+def assert_equal(actual, expected, error_str):
+    """
+    assert_equal
+    """
+
+    if actual != expected:
+        raise TestFailure(error_str)
+
+# ########################################################################### #
 
 @cocotb.coroutine
 def clock_gen(clk, period):
@@ -17,9 +25,9 @@ def clock_gen(clk, period):
     Generate clock
     """
     while True:
-        clk <= 0
+        clk.value = 0
         yield Timer(period)
-        clk <= 1
+        clk.value = 1
         yield Timer(period)
 
 # ########################################################################### #
@@ -31,18 +39,18 @@ def init_clock_reset(clk, reset_n):
     """
     cocotb.fork(clock_gen(clk, 10))
 
-    reset_n = 0
+    reset_n.value = 0
     yield RisingEdge(clk)
     yield FallingEdge(clk)
-    reset_n = 1
+    reset_n.value = 1
     yield FallingEdge(clk)
 
 # ########################################################################### #
 
 @cocotb.test()
-def run_test(dut):
+def test_avalon(dut):
     """
-    run test
+    Test avalon interface
     """
 
     yield init_clock_reset(dut.clk, dut.reset_n)
@@ -54,5 +62,4 @@ def run_test(dut):
     yield avs.write(1, 0xDEADBEEF)
     spi_data = yield avs.read(1)
 
-    if spi_data != 0xDEADBEEF:
-        raise TestFailure("Wrong avalon access")
+    assert_equal(spi_data, 0xDEADBEEF, "Wrong avalon access")
